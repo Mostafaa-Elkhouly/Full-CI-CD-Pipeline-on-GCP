8b5d52860f9eb5c6ff6989ef713d1233342b4d31
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-11-02 09:34:07 +0200

    Add a Complete Flask Python App

--- App/app.py
+++ App/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# two decorators, same function
 @app.route('/')
+@app.route('/index.html')
 def index():
-    return '<h1>Hello From GKE, Mostafa Elkhouly ^_^</h1>'
+    return render_template('index.html', the_title='Tiger Home Page')
+
+@app.route('/symbol.html')
+def symbol():
+    return render_template('symbol.html', the_title='Tiger As Symbol')
+
+@app.route('/myth.html')
+def myth():
+    return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
 
 # main driver function
 if __name__ == "__main__":
