d7240acb106133dac592532b5e4d453bbe620247
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-11-04 20:06:37 +0200

    Updating the Python App

--- App/app.py
+++ App/app.py
@@ -7,16 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', the_title='Tiger Home Page')
-
-@app.route('/symbol.html')
-def symbol():
-    return render_template('symbol.html', the_title='Tiger As Symbol')
-
-@app.route('/myth.html')
-def myth():
-    return render_template('myth.html', the_title='Tiger in Myth and Legend')
-
+    return render_template('index.html', the_title='ICC DevOps Final Project')
 
 # main driver function
 if __name__ == "__main__":
